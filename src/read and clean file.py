3a7e5138f3695b6817297040a866e56371417981
bronbarr <bronwen.barratt@zutari.com>
2022-04-05 14:13:49 +0200

    Add cleaned data frames to a list

--- src/read and clean file.py
+++ src/read and clean file.py
@@ -3,11 +3,13 @@ import glob
 import os
 
 path = 'C:/Users/bronwen.barratt/Desktop/Hackathon/Hackathon_repo/bro_code/data'
-#os.getcwd()
+#Ayesha pls help: os.getcwd() does not work since data is not in the src folder so idk what to do
 input_files = glob.glob(path + '\*.csv')
 
+df_list = []
 for file in input_files:
     file_df = pd.read_csv(file)
     file_df = file_df.iloc[:,:-3]
     file_df.dropna(inplace=True)
+    df_list.append(file_df)
 
