0b9cf21d6674a70ebc51428a0aeb87fa3de94792
bronbarr <bronwen.barratt@zutari.com>
2022-04-06 16:19:21 +0200

    Set up unit tests

---
+++ src/unit tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestClass(unittest.TestCase):
+
+    def test_convert_to_float(self):
+        pass
+
+    def test_clear_nan_rows(self):
+        pass
+
+    def test_get_calculated_area(self):
+        pass
+
+    def test_calculate_total_area(self):
+        pass
+
+    def test_sort_categories(self):
+        pass
