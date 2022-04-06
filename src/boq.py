2041d8a5dd1a7a05abd4ee3b7df616ee77ee8ecd
Ayesha.Jacobs <ayesha.jacobs@zutari.com>
2022-04-06 07:40:08 +0200

    added BOQ class

---
+++ src/boq.py
@@ -0,0 +1,5 @@
+# maybe a maybe a boq writer that both writers inherit from as they will have overlapping properties
+
+class BOQ:
+    def __init__(self):
+        pass
--- src/category.py
+++ src/category.py
@@ -7,12 +7,13 @@ class Category:
     Gets cleaned data in the category, whole row
     Distributes data into categories
     Calculates sum to be put into csv and xlsx
-    maybe category inherits form Duct?
+    maybe category inherits form Duct? - for any duct dataset it can do anything, but for specific
+    category it could do something specific?
     """
 
     def __init__(self, category_number, data):
         self.category_number = category_number  # for if x.category_number == 1, cost..
-        self.data = data  # input data for class
+        self.data = data
 
     def convert_to_float(self):
         """
