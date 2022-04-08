c26843993f33e13a01a747226bbeea19cbcfd4c0
Ayesha.Jacobs <ayesha.jacobs@zutari.com>
2022-04-08 07:51:20 +0200

    get_calculated_area and get_categories unit tests

---
+++ tests/test_get_calculated_area.py
@@ -0,0 +1,12 @@
+import pytest
+import pandas as pd
+from src.duct import Duct
+
+class TestGetCalculatedArea:
+
+    def test_get_calculated_area(self):
+        duct = Duct()
+        calculated_area = duct.get_calculated_area(2, 5, 0)
+        assert calculated_area == 10
+
+
