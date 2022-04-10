7d69181a618f91eb70e30840d2819db1cd5085ff
bronbarr <bronwen.barratt@zutari.com>
2022-04-10 16:36:41 +0200

    Added edge cases for tests

--- tests/test_get_calculated_area.py
+++ tests/test_get_calculated_area.py
@@ -5,9 +5,13 @@ from src.duct import Duct
 
 class TestGetCalculatedArea:
 
-    def test_get_calculated_area(self):
+    def test_get_calculated_area_when_called_returns_correct_area(self):
         duct = Duct()
         calculated_area = duct.get_calculated_area(2, 5, 0)
         assert calculated_area == 10
 
-
+    def test_calculated_area_returns_not_string(self):
+        duct = Duct()
+        calculated_area = duct.get_calculated_area(2, 5, 0)
+        calculated_area_type = type(calculated_area)
+        assert calculated_area_type != str
--- tests/test_sort_data_into_categories.py
+++ tests/test_sort_data_into_categories.py
@@ -4,8 +4,14 @@ from src.category import Category
 
 
 class TestGetCategories:
-    def test_sort_data_into_categories(self):
+    def test_sort_data_into_categories_when_called_returns_correct_category_number(self):
         data = pd.DataFrame([[700, 1000]], columns=['min_width', 'min_height'])
         duct = Category(data)
         category = duct.sort_data_into_categories(data)
         assert category['category'].iloc[0] == 3
+
+    def test_sort_data_into_categories_returns_less_than_five(self):
+        data = pd.DataFrame([[700, 1000]], columns=['min_width', 'min_height'])
+        duct = Category(data)
+        category = duct.sort_data_into_categories(data)
+        assert category['category'].iloc[0] <= 5
